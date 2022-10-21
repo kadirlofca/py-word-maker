3af7213dcc8aa51832a34111cff01e8da319b822
Kadir Lofca <kadirlofca@outlook.com>
2022-10-21 00:49:06 -0400

    Fix: blacklist() used to return a non-tuple variable.
    Added some extra features.
    Added a commented example procedure.

--- word_maker.py
+++ word_maker.py
@@ -2,18 +2,33 @@ import word_maker_library as w
 
 # Here, use provided functions to define rules.
 # Letters in your words will be generated using these rules.
-# Priority is bottom to top. Meaning functions called later can override previous functions.
-def generate_letter(index):
+# Functions called later might override previous functions.
+def generate_letter(index, word_length):
+
+    # Example procedure.
+
+    # Exclude letters.
     w.blacklist(('w', 'x', 'z', 'q', 'j'))
-    w.switch()
+
+    # Select any letter from the alphabet (except the letters blacklisted above).
+    w.any()
+
+    # In half of the length-5 words generated, use switch() for all letters except the third.
+    # The switch() rule makes sure each consonant is followed by a vowel and vice versa. 
+    if word_length == 5 and index != 4 and w.half_words():
+        w.switch()
+
+    # Use switch() for all letters in 4-length words.
+    if word_length == 4:
+        w.switch()
 
     # Force initial letter to be a consonant.
     if index == 0:
         w.con()
 
-# Make words. Pass parameters to set word length and number of words to be generated.
+# Make words. Pass parameters to set min/max word length and number of words to be generated.
 w._generate_letter = generate_letter
-generated_words = w.make_words(5, 50)
+generated_words = w.make_words(4, 5, 12)
 
 for word in generated_words:
     print(word)
--- word_maker_library.py
+++ word_maker_library.py
@@ -1,37 +1,52 @@
 from glob import glob
 import random
+from secrets import randbits
 import numpy
 
-# Alphabet
 _consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
 _vowels = ('a', 'e', 'i', 'o', 'u')
 
-# Private
-_current = ""
-_previous = ""
+_current_letter = ""
+_previous_letter = ""
+_max_words = 5
+_max_letters = 5
+_index_letter = 0
+_index_word = 0
 
-def _set_current(letter):
-    global _current
-    _current = letter
+def _set_current_letter(letter):
+    global _current_letter
+    _current_letter = letter
 
-def alphabet():
-    return _vowels + _consonants
+def half_words():
+    return _index_word > _max_words / 2
+
+def half_letters():
+    return _index_letter > _max_letters / 2
+
+def maybe():
+    return bool(random.getrandbits(1))
 
 def vow():
-    _set_current(random.choice(_vowels))
+    _set_current_letter(random.choice(_vowels))
 
 def con():
-    _set_current(random.choice(_consonants))
+    _set_current_letter(random.choice(_consonants))
 
 def any():
-    _set_current(random.choice(_consonants + _vowels))
+    _set_current_letter(random.choice(_consonants + _vowels))
 
 def switch():
     for v in _vowels:
-        if _previous == v:
+        if _previous_letter == v:
             con()
             return
     vow()
+
+def blacklist(blacklist):
+    global _vowels
+    global _consonants
+    _vowels = tuple(numpy.setdiff1d(_vowels, blacklist))
+    _consonants = tuple(numpy.setdiff1d(_consonants, blacklist))
 
 def reset():
     global _consonants
@@ -39,27 +54,28 @@ def reset():
     _consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
     _vowels = ('a', 'e', 'i', 'o', 'u')
 
-def blacklist(blacklist):
-    global _vowels
-    global _consonants
-    _vowels = numpy.setdiff1d(_vowels, blacklist)
-    _consonants = numpy.setdiff1d(_consonants, blacklist)
-
-def _generate_letter(index):
+def _generate_letter(index, word_length):
     pass
 
-def make_words(word_length, word_count):
+def make_words(word_length_min, word_length_max, word_count):
     words = []
     word = ""
 
-    for w in range(word_count):
+    global _index_word
+    global _max_words
+    _max_words = word_count
+    for _index_word in range(_max_words):
         reset()
 
-        for i in range(word_length):
-            _generate_letter(i)
-            global _previous
-            _previous = _current
-            word += _current 
+        global _index_letter
+        global _max_letters
+        _max_letters = random.randint(word_length_min, word_length_max)
+        for _index_letter in range(_max_letters):
+            _generate_letter(_index_letter, _max_letters)
+            
+            global _previous_letter
+            _previous_letter = _current_letter
+            word += _current_letter 
 
         words += (word, )
         word = ""
